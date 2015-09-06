460ba17acce12acc2118ab045b2e11f066bde8ee
n0p <lbernal@gmail.com>
2015-09-06 19:14:22 +0200

    added /msgs and /help, now stores and checks last update id, done some cleaning

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -6,27 +6,25 @@ class Main extends Controller {
 	{
 		global $config;
 //		$model = $this->loadModel('aprs_model');
-    	//$something = $model->getSomething($id);
 
 		$botID = $config['botID'];
 		$botToken = $config['botToken'];
 		$aprsfiApiKey = $config['aprsfiApiKey'];
+		$lastid = intval(file_get_contents("./last.id"));
 
 		$website = "https://api.telegram.org/bot".$botToken;
 
 		// cuando se configure el webhook poner file_get_contents(php://input)
 
 		//$update = file_get_contents("php://input");
-		$update = file_get_contents($website."/getupdates?offset=606765153");
+		$update = file_get_contents($website."/getupdates?offset=$lastid");
 
 		echo "<pre>";
-		print_r(json_decode($update));
+		//print_r(json_decode($update));
 		$data  = json_decode($update);
 
 		//hacemos la consulta de getupdates
-
-		//la recorremos
-			//para cada mensaje:
+		//la recorremos para cada mensaje:
 
 		foreach ($data->result as $key => $msg){
 
@@ -37,13 +35,8 @@ class Main extends Controller {
 
 			echo "mensaje a grupo :". intval($in_group) ."<br>";
 
-
-				// only in groups
-
-					//comprobar que estamos en un grupo
-
+			//comprobar que estamos en un grupo
 			//if (!$in_group) continue;
-
 
 			$group_id 	= $msg->message->chat->id;
 			$from_id	= $msg->message->from->id;
@@ -51,54 +44,14 @@ class Main extends Controller {
 			if (!isset($msg->message->text)) continue;
 			$text 		= $msg->message->text;
 
-	/*
-
-stdClass Object
-(
-    [ok] => 1
-    [result] => Array
-        (
-            [0] => stdClass Object
-                (
-                    [update_id] => 911242039
-                    [message] => stdClass Object
-                        (
-                            [message_id] => 63
-                            [from] => stdClass Object
-                                (
-                                    [id] => 8908013
-                                    [first_name] => Guillermo - Killer
-                                    [username] => killer415
-                                )
-
-                            [chat] => stdClass Object
-                                (
-                                    [id] => -12658615
-                                    [title] => Airsoft partidas club
-                                )
-
-                            [date] => 1441465223
-                            [text] => /crearpartida
-                        )
-
-                )
-
-*/
-
-
 //rutina:
-
-
 	//si el mensaje concuerda con alguno de estos hacer lo que se tenga que hacer
-
-
-	
 
 	// admin commands
 	$commands = array(
-			'asdf' => '/(\/asdf )$/',
+			'help' => '/(\\/help)$/',
 			'last' => '/(\\/last)\\s+(\S+)+/',
-			'cancelarpartida' => '/(\/cancelarpartida)$/',
+			'msgs' => '/(\\/msgs)\\s+(\S+)+/',
 		);
 
 		$matches = null;
@@ -111,48 +64,51 @@ stdClass Object
 			if (!empty($matches)) {
 
 				switch($commandkey){
-					case "asdf":
+					case "help":
 						echo "comando $commandkey<br>";
-						$teclado = json_encode(array(array('/asdf 01-09-2015')));
-						file_get_contents($website."/sendMessage?chat_id=$group_id&text=@$from_username Para crear partida usa /crearpartida dd-mm-aaaa.&reply_markup=$teclado");
+						file_get_contents($website."/sendMessage?chat_id=$group_id&text=Comands:");
+						file_get_contents($website."/sendMessage?chat_id=$group_id&text=/last ID - last beacon from ID");
+						file_get_contents($website."/sendMessage?chat_id=$group_id&text=/msgs ID - show last 10 messages from ID");
 					break;
 
 					case "last":
 						echo "comando $commandkey<br>";
-						echo "<pre>";
 						
 						$indicativo = $matches[2];
 						echo "last: ".$indicativo;
 						$result = file_get_contents("http://api.aprs.fi/api/get?name=$indicativo&what=loc&apikey=$aprsfiApiKey&format=json");
 						
-						print_r($result);
+						//print_r($result);
 						$location = json_decode($result);
-						print_r($location);
+						//print_r($location);
 						file_get_contents($website."/sendLocation?chat_id=$group_id&latitude=".$location->entries[0]->lat."&longitude=".$location->entries[0]->lng);
 						file_get_contents($website."/sendMessage?chat_id=$group_id&text=UTC:".gmdate("Y-m-d H:i:s",$location->entries[0]->lasttime)." - ".$location->entries[0]->comment);
-
+						
 					break;
 
-					case "cancelarpartida":
+					case "msgs":
 						echo "comando $commandkey<br>";
 						
-//						$error = $model->cancelarpartida($group_id);
-						file_get_contents($website."/sendMessage?chat_id=$group_id&text=Partida cerrada.");
+						$indicativo = $matches[2];
+						echo "msgs: ".$indicativo;
+						$result = file_get_contents("http://api.aprs.fi/api/get?what=msg&dst=$indicativo&apikey=$aprsfiApiKey&format=json");
 						
-
+						//print_r($result);
+						$msgs = json_decode($result);
+						//print_r($msgs);
+						foreach($msgs->entries as $msg) {
+							file_get_contents($website."/sendMessage?chat_id=$group_id&text=>".gmdate("Y-m-d H:i:s",$msg->time).
+							": ".$msg->srccall." -> ".$msg->dst.": ".$msg->message);
+						}
+						
 					break;
-					
-
 				}
-
 			}
-
 			$matches = null;
-
-
+			$lastid = $last_update_id+1;
 		}
 	}
+	file_put_contents("./last.id",$lastid);
 	}
 }
-
 ?>

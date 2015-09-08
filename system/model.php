a7bcd4f1db991a16a313d3278035c5cb96c0365d
quorth <santi.nieve@gmail.com>
2015-09-08 15:56:35 +0200

    Changed Model class with SQLite3 functions

--- system/model.php
+++ system/model.php
@@ -2,26 +2,25 @@
 
 class Model {
 
-	//private $connection;
+	private $connection;
+
 	public function __construct()
 	{
 		global $config;
 		
 	//	$this->connection = mysql_pconnect($config['db_host'], $config['db_username'], $config['db_password']) or die('MySQL Error: '. mysql_error());
 	//	mysql_select_db($config['db_name'], $this->connection);
-	
-	//$this->open('aprsbot.db');
-	
+		$this->connection = new SQLite3(APP_DIR.'data.db');
 	}
 
 	public function escapeString($string)
 	{
-		return mysql_real_escape_string($string);
+		return $this->connection->escapeString($string);
 	}
 
 	public function escapeArray($array)
 	{
-	    array_walk_recursive($array, create_function('&$v', '$v = mysql_real_escape_string($v);'));
+	    array_walk_recursive($array, create_function('&$v', '$v = $this->connection->escapeString($v);'));
 		return $array;
 	}
 	
@@ -47,17 +46,17 @@ class Model {
 	
 	public function query($qry)
 	{
-		//$result = query($qry) or die('SQL Error: '. lastErrorMsg());
+		$result = $this->connection->query($qry) or die('SQLite Error: '. $this->connection->lastErrorMsg());
 		$resultObjects = array();
 
-		//while($row = fetchArray($result)) $resultObjects[] = $row;
+		while($row = $result->fetchArray())	 $resultObjects[] = $row;
 
 		return $resultObjects;
 	}
 
 	public function execute($qry)
 	{
-		//$exec = query($qry) or die('SQL Error: '. lastErrorMsg());
+		$exec = $this->connection->exec($qry) or die('SQLite Error: '. $this->connection->lastErrorMsg());
 		return $exec;
 	}
     
